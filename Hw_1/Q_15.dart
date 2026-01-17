8f30a10e8534d028ddc68d9d601cef3672967bc3
Marioma Ebrahim <mariomaebrahim784@gmail.com>
2026-01-17 02:14:28 +0200

    Update Q_15.dart

---
+++ Hw_1/Q_15.dart
@@ -0,0 +1,6 @@
+//What does it mean for Dart to be a statically-typed language, and how does it
+//impact the way variables are used?
+
+//which means the type of a variable is known at compile time.
+//This ensures variables cannot change their type after being declared, reducing errors and making code safer and more readable.
+
