42573d1ca246a975a763e5fd9bb58883ac0d08cb
Marioma Ebrahim <mariomaebrahim784@gmail.com>
2026-01-20 17:05:35 +0200

    update ex_9.dart

--- HW_2/ex_9.dart
+++ HW_2/ex_9.dart
@@ -1,8 +1,13 @@
 // ignore_for_file: unused_local_variable
 
-void main(){
+void main() {
   List<Map<String, dynamic>> students = [
-  {'name': 'Youssef', 'grade': 85},
- {'name': 'mero', 'grade': 90},
-];
+    {'name': 'Youssef', 'grade': 85},
+    {'name': 'mero', 'grade': 90},
+  ];
+  print(students[1]['grade']);
+  int totalGrades = students[0]['grade'] + students[1]['grade'];
+  double average = totalGrades / students.length;
+  print(average);
+
 }
